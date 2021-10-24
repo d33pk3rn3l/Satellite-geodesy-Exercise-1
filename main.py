5e8c0944d6dcab06cffcd433cd0fc6baf2373c1d
andreaspohl <andreas.g.pohl>
2021-10-24 21:13:44 +0200

    task 3 fixed

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ Beidou07    = np.loadtxt('Data/PC07asc.sec')
 Lageos1     = np.loadtxt('Data/PL52asc.sec')
 
 satellites = {"GPS07": GPS07, "GLONASS07": GLONASS07, "GALILEO07": GALILEO07, "QZSS03": GALILEO07, "Beidou07": Beidou07, "Lageos1": Lageos1}
+#satellites = {"GLONASS07": GLONASS07}
 
-tasks.task1(satellites)
-tasks.task2(satellites)
+#tasks.task1(satellites)
+#tasks.task2(satellites)
 tasks.task3(satellites)
-tasks.task4(satellites)
+#tasks.task4(satellites)
